b45a20d21f968e8862c5cddc615e1dddaadd8294
pabloba <pablo.arcati@nftydoor.com>
2025-05-26 21:55:10 -0300

    fix

--- Semana de integracion 2/utils/mainMenu.py
+++ Semana de integracion 2/utils/mainMenu.py
@@ -19,6 +19,45 @@ def main_loop():
         opcion = menu()
         
         if opcion == "1":
+            '''una función que le solicite al usuario ingresar la cantidad de integrantes que desea trabajar, y luego le solicite los DNI
+            en base a ese FOR vas a sacar 2 variables, una que sea de tipo set, y otra que sea de tipo list. 
+            La set, va a pasar por el normlizador limpiar_y_convertir porque devuelve un conjunto de numeros enteros, y la lista tiene que pasar por otro normalizador pero que devuelva una lista de numeros enteros
+            Mostrar un menu, que diga, que quiere hacer con esos conjuntos? Si quiere union, intersección, diferencia o diferencia simétrica.
+            Union, cantidad de sets: Ej:
+            [
+                {0, 1, 2, 3, 4, 5, 9},
+                {2, 3, 4, 8, 9},
+                {3, 4, 5, 6, 7, 8}
+            ]
+            setters = []
+            listasDni = []
+            for i in range(cantidad_integrantes):
+                dni = input(f"Ingrese el DNI del integrante {i + 1}: ")
+                conjunto_set = limpiar_y_convertir(dni)
+                conjunto_list = normalizar_lista(dni)
+                setters.append(conjunto_set)
+                listasDni.append(conjunto_list)
+            
+            menu, opciones = {
+                "1": "Unión de conjuntos",
+                "2": "Intersección de conjuntos",
+                "3": "Diferencia de conjuntos",
+                "4": "Diferencia simétrica de conjuntos",
+                "5": "Salir"
+            }
+            si selecciona 1:
+            
+            def union_varios(conjuntos):
+            unionConjuntos = set()
+            for i in range(len(conjuntos) - 1):
+                unionConjuntos = conjuntos[i].union(conjuntos[i + 1])
+            return print(f"Unión de conjuntos: {unionConjuntos}")
+
+            P.union(R).union(E)
+            P.intersection(R).intersection(E)
+            P.difference(R).difference(E)
+            P.symmetric_difference(R).symmetric_difference(E)
+            '''
             ejecutar_submenu_conjuntos()
         
         elif opcion == "2":
@@ -38,8 +77,6 @@ def ejecutar_submenu_conjuntos():
         if sub_option == "1":
             mostrar_venn_con_elementos(P, R)
         elif sub_option == "2":
-            '''pedir al usuario que ingrese dos conjuntos y mostrar la diferencia entre ellos'''
-            '''print la diferencia. y dejar una opcion que diga, si quiere ver el grafico.'''
             diferencia_entre_dos(P, R)
         elif sub_option == "3":
             union_varios([P, R])
--- Semana de integracion 2/utils/normalizador.py
+++ Semana de integracion 2/utils/normalizador.py
@@ -7,3 +7,12 @@ def limpiar_y_convertir(cadena):
     cadena = re.sub(regex, ',',cadena)
     # Dividimos por comas y convertimos a enteros
     return {int(num) for num in cadena.split(',') if num.strip().isdigit()}
+
+def normalizar_lista(cadena):
+    # Reemplazamos . y - por comas para unificar
+    regex = r"[a-zA-Z]+"
+
+    cadena = cadena.replace('.', ',').replace('-', ',')
+    cadena = re.sub(regex, ',',cadena)
+    # Dividimos por comas y convertimos a enteros
+    return [int(num) for num in cadena.split(',') if num.strip().isdigit()]
