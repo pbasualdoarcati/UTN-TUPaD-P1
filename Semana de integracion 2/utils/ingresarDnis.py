a573cc06689aa16361c5f2c67127a22bbd374f71
pabloba <pablo.arcati@nftydoor.com>
2025-06-02 21:04:12 -0300

    Add verification

--- Semana de integracion 2/utils/ingresarDnis.py
+++ Semana de integracion 2/utils/ingresarDnis.py
@@ -3,7 +3,11 @@
 def ingresar_dnis(cant_integrantes):
     dnis_list = []
     for i in range(cant_integrantes):
-        dni = int(input(f"Ingrese el DNI del integrante {i + 1}: "))
+        dni_input = input(f"Ingrese el DNI del integrante {i + 1}: ")
+        while not dni_input.isdigit() or int(dni_input) < 1000000 or int(dni_input) > 99999999:
+            print("DNI inválido. Debe tener entre 7 y 8 dígitos y solo contener números.")
+            dni_input = input(f"Ingrese el DNI del integrante {i + 1} nuevamente: ")
+        dni = int(dni_input)
         dnis_list.append(dni)
     return dnis_list
 
